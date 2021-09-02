f0384ca738f059623919873f88e463ca8945d434
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-09-02 07:46:14 -0300

    Incrementando detalhe do pedido

--- dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
+++ dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ButtonSimples from '../../components/Button/Simples'
 import Titulo from '../../components/Texto/Titulo'
+import { TextoDados } from '../../components/Texto/Dados'
+import { TabelaSimples } from '../../components/Tabela/Simples'
 
 class DetalhesDoPedido extends Component {
   renderCabecalho() {
@@ -20,8 +22,84 @@ class DetalhesDoPedido extends Component {
     )
   }
 
+  renderDadosDoCliente() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados do Cliente" />
+        <br />
+        <TextoDados chave="Nome" valor="Cliente 1" />
+        <TextoDados chave="CPF" valor="111.222.33345" />
+        <TextoDados chave="Telefone" valor="11 1234-5678" />
+        <TextoDados chave="Data de Nascimento" valor="01/01/1970" />
+      </div>
+    )
+  }
+
+  renderDadosDeEntrega() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados de Entrega" />
+        <br />
+        <TextoDados chave="Endereco" valor="Rua Teste, 123" />
+        <TextoDados chave="Bairro" valor="Centro" />
+        <TextoDados chave="Cidade" valor="São Paulo" />
+        <TextoDados chave="Estado" valor="SP" />
+        <TextoDados chave="CEP" valor="01234-567" />
+      </div>
+    )
+  }
+
+  renderDadosDePagamento() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados do Pagamento" />
+        <br />
+        <TextoDados chave="Taxa de Entrega" valor="R$ 15,50 (PAC)" />
+        <TextoDados chave="Valor do Pedido" valor="R$ 32,00" />
+        <TextoDados chave="Valor Total" valor="R$ 47,50" />
+        <TextoDados chave="Forma de Pagamento" valor="BOLETO" />
+      </div>
+    )
+  }
+
+  renderDadosDoCarrinho() {
+    const dados = [
+      {
+        Produto: 'Produto 1',
+        'Preço Und.': 'R$ 12,00',
+        Quantidade: '1',
+        'Preço Total': 'R$ 12,00',
+      },
+      {
+        Produto: 'Produto 2',
+        'Preço Und.': 'R$ 10,00',
+        Quantidade: '2',
+        'Preço Total': 'R$ 20,00',
+      },
+    ]
+
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Carrinho" />
+        <br />
+        <TabelaSimples
+          cabecalho={['Produto', 'Preço Und.', 'Quantidade', 'Preço Total']}
+          dados={dados}
+        />
+      </div>
+    )
+  }
+
   render() {
-    return <div className="Detalhes-do-Pedido">{this.renderCabecalho()}</div>
+    return (
+      <div className="Detalhes-do-Pedido">
+        {this.renderCabecalho()}
+        {this.renderDadosDoCliente()}
+        {this.renderDadosDoCarrinho()}
+        {this.renderDadosDeEntrega()}
+        {this.renderDadosDePagamento()}
+      </div>
+    )
   }
 }
 

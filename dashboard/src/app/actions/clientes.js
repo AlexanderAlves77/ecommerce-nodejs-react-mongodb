8c6d723e90509c6454517209a410e5dc4f9641b1
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-09-29 21:26:08 -0300

    Detalhes do clientes preparando actions, reducer e configurações e index

--- dashboard/src/app/actions/clientes.js
+++ dashboard/src/app/actions/clientes.js
@@ -2,7 +2,14 @@ import { getHeaders } from './localStorage'
 import axios from 'axios'
 import { api, versao } from '../config'
 import errorHandling from './errorHandling'
-import { GET_CLIENTES } from './types'
+import { transformeDate } from './index'
+import {
+  GET_CLIENTES,
+  GET_CLIENTE,
+  GET_CLIENTE_PEDIDOS,
+  LIMPAR_CLIENTE,
+  REMOVE_CLIENTE,
+} from './types'
 
 export const getClientes = (atual, limit, loja) => {
   return function (dispatch) {
@@ -22,12 +29,89 @@ export const getClientesPesquisa = (termo, atual, limit, loja) => {
   return function (dispatch) {
     axios
       .get(
-        `${api}/${versao}/api//search/${termo}?offset=${atual}&limit=${limit}&loja=${loja}`,
+        `${api}/${versao}/api/search/${termo}?offset=${atual}&limit=${limit}&loja=${loja}`,
         getHeaders()
       )
       .then(response =>
         dispatch({ type: GET_CLIENTES, payload: response.data })
       )
       .catch(errorHandling)
+  }
+}
+
+export const getCliente = (id, loja) => {
+  return function (dispatch) {
+    axios
+      .get(
+        `${api}/${versao}/api/clientes/admin/${id}?loja=${loja}`,
+        getHeaders()
+      )
+      .then(response => dispatch({ type: GET_CLIENTE, payload: response.data }))
+      .catch(errorHandling)
+  }
+}
+
+export const getClientePedidos = (id, atual, limit, loja) => {
+  return function (dispatch) {
+    axios
+      .get(
+        `${api}/${versao}/api/clientes/admin/${id}/pedidos?loja=${loja}&offset=${atual}&limit=${limit}`,
+        getHeaders()
+      )
+      .then(response =>
+        dispatch({ type: GET_CLIENTE_PEDIDOS, payload: response.data })
+      )
+      .catch(errorHandling)
+  }
+}
+
+export const limparCliente = () => ({ type: LIMPAR_CLIENTE })
+
+export const updateCliente = (cliente, id, loja, cb) => {
+  return function (dispatch) {
+    axios
+      .put(
+        `${api}/${versao}/api/clientes/admin/${id}?loja=${loja}`,
+        {
+          nome: cliente.nome,
+          cpf: cliente.cpf,
+          email: cliente.email,
+          telefones: cliente.telefones,
+          endereco: {
+            local: cliente.endereco,
+            numero: cliente.numero,
+            bairro: cliente.bairro,
+            cidade: cliente.cidade,
+            estado: cliente.estado,
+            CEP: cliente.cep,
+          },
+          dataDeNascimento: transformeDate(
+            cliente.dataDeNascimento,
+            '/',
+            'YYYY-MM-DD'
+          ),
+        },
+        getHeaders()
+      )
+      .then(response => {
+        dispatch({ type: GET_CLIENTE, payload: response.data })
+        cb(null)
+      })
+      .catch(e => cb(errorHandling(e)))
+  }
+}
+
+export const removerCliente = (id, loja, cb) => {
+  return function (dispatch) {
+    axios
+      .delete(
+        `${api}/${versao}/api/clientes/admin/${id}?loja=${loja}`,
+        getHeaders()
+      )
+      .then(response => {
+        dispatch({ type: REMOVE_CLIENTE, payload: response.data })
+        cb(null)
+      })
+      .catch(e => cb(errorHandling(e)))
   }
 }
--- dashboard/src/app/containers/Cliente/index.js
+++ dashboard/src/app/containers/Cliente/index.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { actions } from '../../actions/clientes'
 
 import DetalhesDoCliente from './detalhesDoCliente'
 import DetalhesDosPedidos from './detalhesDosPedidos'
 
 class Cliente extends Component {
+  componentDidMount() {
+    const { usuario } = this.props
+    const { id } = this.props.match.params
+    if (!usuario) return null
+    this.props.getCliente(id, usuario.loja)
+  }
+
+  componentWillUnmount() {
+    this.props.limparCliente()
+  }
+
   render() {
     return (
       <div className="Cliente full-width flex vertical">
         <div className="Card">
-          <DetalhesDoCliente />
+          <DetalhesDoCliente history={this.props.history} />
         </div>
         <div className="Sub-Card">
-          <DetalhesDosPedidos />
+          <DetalhesDosPedidos id={this.props.match.params.id} />
         </div>
       </div>
     )
   }
 }
 
-export default Cliente
+const mapStateToProps = state => ({
+  usuario: state.auth.usuario,
+})
+
+export default connect(mapStateToProps, actions)(Cliente)

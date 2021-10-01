ad75406182d4ad48ff26d2b8121a318b50a9751e
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-01 19:18:20 -0300

    Preparando página de categorias e nova categoria

---
+++ dashboard/src/app/actions/categorias.js
@@ -0,0 +1,35 @@
+import { getHeaders } from './localStorage'
+import axios from 'axios'
+import { api, versao } from '../config'
+import errorHandling from './errorHandling'
+import { GET_CATEGORIAS, GET_CATEGORIA } from './types'
+
+export const getCategorias = loja => {
+  return function (dispatch) {
+    axios
+      .get(`${api}/${versao}/api/categorias?loja=${loja}`, getHeaders())
+      .then(response =>
+        dispatch({ type: GET_CATEGORIAS, payload: response.data })
+      )
+      .catch(errorHandling)
+  }
+}
+
+export const salvarCategoria = (categoria, loja, cb) => {
+  return function (dispatch) {
+    axios
+      .post(
+        `${api}/${versao}/api/categorias?loja=${loja}`,
+        {
+          nome: categoria.nome,
+          codigo: categoria.codigo,
+        },
+        getHeaders()
+      )
+      .then(response => {
+        dispatch({ type: GET_CATEGORIAS, payload: response.data })
+        cb(null)
+      })
+      .catch(e => cb(errorHandling(e)))
+  }
+}
---
+++ dashboard/src/app/reducers/categoria_reducer.js
@@ -0,0 +1,18 @@
+import { GET_CATEGORIAS, GET_CATEGORIA } from '../actions/types'
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case GET_CATEGORIAS:
+      return {
+        ...state,
+        categorias: action.payload.categorias,
+      }
+    case GET_CATEGORIA:
+      return {
+        ...state,
+        categoria: action.payload.categoria,
+      }
+    default:
+      return state
+  }
+}

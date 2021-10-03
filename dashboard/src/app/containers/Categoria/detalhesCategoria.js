9849469bc5805fd2d7510637bffbed7398e54d72
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-03 12:31:24 -0300

    Configuração inicial do index de detalhes de categoria

--- dashboard/src/app/containers/Categoria/detalhesCategoria.js
+++ dashboard/src/app/containers/Categoria/detalhesCategoria.js
@@ -6,12 +6,79 @@ import Titulo from '../../components/Texto/Titulo'
 import InputSelect from '../../components/Inputs/Select'
 
 import Voltar from '../../components/Links/Voltar'
+import { connect } from 'react-redux'
+import AlertGeral from '../../components/Alert/Geral'
+import * as actions from '../../actions/categorias'
 
 class DetalhesCategoria extends Component {
-  state = {
-    nome: 'Categoria',
-    disponibilidade: 'disponível',
-    codigo: 'categoria',
+  generateStateCategoria = props => ({
+    nome: props.categoria ? props.categoria.nome : '',
+    disponibilidade: props.categoria
+      ? props.categoria.disponibilidade ||
+        props.categoria.disponibilidade === undefined
+        ? 'disponivel'
+        : 'indisponivel'
+      : '',
+    codigo: props.categoria ? props.categoria.codigo : '',
+  })
+
+  constructor(props) {
+    super()
+    this.state = {
+      ...this.generateStateCategoria(props),
+      erros: {},
+      aviso: null,
+    }
+  }
+
+  componentDidUpdate(nextProps) {
+    if (
+      (!this.props.categoria && nextProps.categoria) ||
+      (this.props.categoria &&
+        nextProps.categoria &&
+        this.props.updatedAt !== nextProps.categoria.updatedAt)
+    )
+      this.setState(this.generateStateCategoria(nextProps))
+  }
+
+  salvarCategoria() {
+    const { usuario, categoria } = this.props
+    if (!usuario || !categoria) return null
+
+    this.props.updateCategoria(
+      this.state,
+      categoria._id,
+      usuario.loja,
+      error => {
+        this.setState({
+          aviso: {
+            status: !error,
+            msg: error ? error.message : 'Categoria atualizada com sucesso',
+          },
+        })
+      }
+    )
+  }
+
+  removerCategoria() {
+    const { usuario, categoria } = this.props
+    if (!usuario || !categoria) return null
+
+    if (!window.confirm('Você realmente deseja remover está categoria?')) return
+
+    this.props.updateCategoria(
+      this.state,
+      categoria._id,
+      usuario.loja,
+      error => {
+        this.setState({
+          aviso: {
+            status: !error,
+            msg: error ? error.message : 'Categoria atualizada com sucesso',
+          },
+        })
+      }
+    )
   }
 
   renderCabecalho() {
@@ -89,7 +156,8 @@ class DetalhesCategoria extends Component {
   render() {
     return (
       <div className="Detalhes-Categoria">
-        <Voltar path="/categorias" />
+        <Voltar history={this.props.history} />
+        <AlertGeral aviso={this.state.aviso} />
         {this.renderCabecalho()}
         {this.renderDados()}
       </div>
@@ -97,4 +165,9 @@ class DetalhesCategoria extends Component {
   }
 }
 
-export default DetalhesCategoria
+const mapStateToProps = state => ({
+  categoria: state.categoria.categoria,
+  usuario: state.auth.usuario,
+})
+
+export default connect(mapStateToProps, actions)(DetalhesCategoria)
--- dashboard/src/app/containers/Categoria/index.js
+++ dashboard/src/app/containers/Categoria/index.js
@@ -3,13 +3,27 @@ import React, { Component } from 'react'
 import DetalhesCategoria from './detalhesCategoria'
 import ListaDeProdutos from './listaDeProdutos'
 
+import { connect } from 'react-redux'
+import * as actions from '../../actions/categorias'
+
 class Categoria extends Component {
+  componentDidMount() {
+    const { usuario } = this.props
+    const { id } = this.props.match.params
+    if (!usuario || !id) return null
+    this.props.getCategoria(id, usuario.loja)
+  }
+
+  componentDidUnmount() {
+    this.props.limparCategoria()
+  }
+
   render() {
     return (
       <div className="Categoria full-width">
         <div className="Card">
           <div>
-            <DetalhesCategoria />
+            <DetalhesCategoria history={this.props.history} />
           </div>
 
           <div>
@@ -21,4 +35,8 @@ class Categoria extends Component {
   }
 }
 
-export default Categoria
+const mapStateToProps = state => ({
+  usuario: state.auth.usuario,
+})
+
+export default connect(mapStateToProps, actions)(Categoria)

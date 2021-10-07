114b023bfda6e9eb8fbe452179cd6f2f0e2e8ef1
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-07 08:46:30 -0300

    Criando página de novo produto

---
+++ dashboard/src/app/containers/Produtos/novoProduto.js
@@ -0,0 +1,198 @@
+import React, { Component } from ' react'
+import Titulo from '../../components/Texto/Titulo'
+import Button from '../../components/Button/Simples'
+import { TextoDados } from '../../components/Texto/Dados'
+import InputSelect from '../../components/Inputs/Select'
+import InputSimples from '../../components/Inputs/Simples'
+import Voltar from '../../components/Links/Voltar'
+
+import { connect } from 'react-redux'
+import * as actionsProdutos from '../../actions/produtos'
+import * as actionsCategorias from '../../actions/categorias'
+import AlertGeral from '../../components/Alert/Geral'
+
+class NovoProduto extends Component {
+  state = {
+    nome: '',
+    descricao: '',
+    categoria: '',
+    preco: 0,
+    promocao: 0,
+    sku: '',
+    aviso: null,
+    erros: {},
+  }
+
+  getCategorias(props) {
+    const { usuario, getCategorias } = props
+    if (usuario) getCategorias(usuario.loja)
+  }
+
+  componentDidMount() {
+    this.getCategorias(this.props)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.usuario && this.props.usuario) this.getCategorias(this.props)
+  }
+
+  validate() {
+    const { nome, descricao, categoria, preco, sku } = this.state
+    const erros = {}
+
+    if (!nome) erros.nome = 'Preencha aqui com o nome do produto'
+    if (!categoria) erros.categoria = 'Preencha aqui com a categoria do produto'
+    if (!descricao) erros.descricao = 'Preencha aqui com a descricao do produto'
+    if (!preco) erros.preco = 'Preencha aqui com o preco do produto'
+    if (!sku) erros.sku = 'Preencha aqui com o SKU do produto'
+
+    this.setState({ erros })
+    return !(Object.keys(erros).length > 0)
+  }
+
+  salvarProduto() {
+    const { usuario, salvarProduto } = this.props
+    if (!usuario) return null
+    if (!this.validate()) return null
+    salvarProduto(this.state, usuario.loja, error => {
+      this.setState({
+        aviso: {
+          status: !error,
+          msg: error ? error.message : 'Produto criado com sucesso!',
+        },
+      })
+    })
+  }
+
+  renderCabecalho() {
+    const { nome } = this.state
+
+    return (
+      <div className="flex">
+        <div className="flex-1 flex vertical">
+          <Titulo tipo="h1" titulo={nome || 'Novo Produto'} />
+        </div>
+        <div className="flex-1 flex flex-end">
+          <Button
+            type="success"
+            label="Salvar"
+            onClick={() => this.salvarProduto()}
+          />
+        </div>
+      </div>
+    )
+  }
+
+  onChangeInput = (field, value) => this.setState({ [field]: value })
+
+  renderDados() {
+    const { nome, descricao, categoria, preco, promocao, sku, erros } =
+      this.state
+    const { categorias } = this.props
+
+    return (
+      <div className="Dados-Produto">
+        <InputSimples
+          name="nome"
+          label="Nome:"
+          value={nome}
+          error={erros.nome}
+          onChange={evt => this.onChangeInput('nome', evt.target.value)}
+        />
+        <br />
+        <TextoDados
+          chave="Categoria"
+          vertical
+          valor={
+            <InputSelect
+              name="categoria"
+              onChange={evt =>
+                this.onChangeInput('categoria', evt.target.value)
+              }
+              value={categoria}
+              error={erros.categoria}
+              opcoes={[
+                { label: 'Selecionar...', value: '' },
+                ...(categorias || []).map(item => ({
+                  label: item.nome,
+                  value: item._id,
+                })),
+              ]}
+            />
+          }
+        />
+        <br />
+        <TextoDados
+          chave="Descricao"
+          valor={
+            <div>
+              <textarea
+                name="descricao"
+                onChange={evt =>
+                  this.onChangeInput('categdescricaooria', evt.target.value)
+                }
+                value={descricao}
+                rows="10"
+                style={{ resize: 'nome' }}
+              />
+              {erros.descricao && (
+                <small className="small-danger">{erros.descricao}</small>
+              )}
+            </div>
+          }
+        />
+        <InputSimples
+          name="preco"
+          label="Preço:"
+          type="number"
+          value={preco}
+          error={erros.preco}
+          onChange={evt => this.onChangeInput('preco', evt.target.value)}
+        />
+        <InputSimples
+          name="promocao"
+          label="Valor em Promoção:"
+          type="number"
+          value={promocao}
+          error={erros.promocao}
+          onChange={evt => this.onChangeInput('promocao', evt.target.value)}
+        />
+        <InputSimples
+          name="sku"
+          label="SKU:"
+          value={sku}
+          error={erros.sku}
+          onChange={evt => this.onChangeInput('sku', evt.target.value)}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="Novo-Produto full-width">
+        <div className="Card">
+          <Voltar history={this.props.history} />
+          {this.renderCabecalho()}
+          <AlertGeral aviso={this.state.aviso} />
+          <br />
+          <div className="flex-horizontal">
+            <div className="flex-1 flex vertical">{this.renderDados()}</div>
+            <div className="flex-1 flex vertical"></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  produto: state.produto.produto,
+  categorias: state.categoria.categorias,
+  usuario: state.auth.usuario,
+})
+
+export default connect(mapStateToProps, {
+  ...actionsCategorias,
+  ...actionsProdutos,
+})(NovoProduto)

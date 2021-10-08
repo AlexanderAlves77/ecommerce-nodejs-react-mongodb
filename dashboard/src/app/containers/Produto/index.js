83460703423b6392ee937816e78c916a04c3f409
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-08 20:55:25 -0300

    Preparando actions e reducers em detalhes do produto

--- dashboard/src/app/containers/Produto/index.js
+++ dashboard/src/app/containers/Produto/index.js
@@ -3,12 +3,24 @@ import React, { Component } from 'react'
 import DetalhesProduto from './detalhesProduto'
 import DetalhesVariacoes from './detalhesVariacoes'
 
+import { connect } from 'react-redux'
+import * as actionsProdutos from '../../actions/produtos'
+import * as actionsCategorias from '../../actions/categorias'
+
 class Produto extends Component {
+  componentDidMount() {
+    const { usuario, getProduto, getCategorias } = this.props
+    if (!usuario) return
+    const { id } = this.props.match.params
+    getProduto(id, usuario.loja)
+    getCategorias(usuario.loja)
+  }
+
   render() {
     return (
       <div className="Produto full-width flex vertical">
         <div className="Card">
-          <DetalhesProduto />
+          <DetalhesProduto history={this.props.history} />
         </div>
         <div>
           <DetalhesVariacoes />
@@ -18,4 +30,11 @@ class Produto extends Component {
   }
 }
 
-export default Produto
+const mapStateToProps = state => ({
+  usuario: state.auth.usuario,
+})
+
+export default connect(mapStateToProps, {
+  ...actionsProdutos,
+  ...actionsCategorias,
+})(Produto)

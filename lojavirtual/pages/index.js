2269885261dff8ddd8d859bdddeac41aaa3d1f52
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-26 09:35:27 -0300

    Iniciando o Next.js ao projeto da Loja Virtual

--- lojavirtual/pages/index.js
+++ lojavirtual/pages/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Link from 'next/link'
 
 export default class Index extends Component {
+  static async getInitialProps() {
+    console.log('loaded')
+    return {}
+  }
+
   render() {
     return (
       <div>
         <h2>Loja IT</h2>
+        <br />
+        <a href="/">Link padrão</a>
+        <Link href="/">
+          <span>Link via Next</span>
+        </Link>
       </div>
     )
   }
---
+++ lojavirtual/pages/produto/[produto].js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Produto extends Component {
+  static async getInitialProps({ query }) {
+    const { produto } = query
+    console.log(produto)
+    return { produto }
+  }
+
+  render() {
+    const { produto } = this.props
+    return (
+      <div>
+        <h2>Loja IT - Produto - {produto}</h2>
+      </div>
+    )
+  }
+}

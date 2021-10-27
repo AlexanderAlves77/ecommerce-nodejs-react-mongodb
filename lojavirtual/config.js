e8b413dd48c3b1f91e0d6a805926281453c0f9d0
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-10-27 09:51:10 -0300

    Criando pastas e arquivos da loja

---
+++ lojavirtual/config.js
@@ -0,0 +1,4 @@
+export const API = 'http://localhost:3000',
+  versao = 'v1',
+  loja = '5c253046a20fd9faa44b52wm',
+  baseImg = API + '/public/images/'
--- lojavirtual/pages/index.js
+++ lojavirtual/pages/index.js
@@ -2,20 +2,12 @@ import React, { Component } from 'react'
 import Link from 'next/link'
 
 export default class Index extends Component {
-  static async getInitialProps() {
-    console.log('loaded')
-    return {}
-  }
-
   render() {
     return (
       <div>
         <h2>Loja IT</h2>
         <br />
         <a href="/">Link padrão</a>
-        <Link href="/">
-          <span>Link via Next</span>
-        </Link>
       </div>
     )
   }

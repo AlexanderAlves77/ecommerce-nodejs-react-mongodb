66eb4608f1aee7a73d43686c588b9ec984402d42
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-11-09 09:14:41 -0300

    Configurando o estilo da loja e da página inicial

--- lojavirtual/components/Style.js
+++ lojavirtual/components/Style.js
@@ -8,12 +8,66 @@ export default () => (
         padding: 0;
       }
 
+      body,
+      * {
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      }
+
       .flex {
         display: flex;
       }
+
+      .flex-1 {
+        flex: 1;
+      }
+      .flex-2 {
+        flex: 2;
+      }
+      .flex-3 {
+        flex: 3;
+      }
+
       .flex-center {
         justify-center: center;
         align-items: center;
+      }
+
+      .Header {
+        max-height: 100px;
+      }
+
+      .header-wrapper {
+        max-width: 1200px;
+        margin: 0 auto;
+        width: 100%;
+        display: flex;
+      }
+
+      @media screen and (max-width: 720px) {
+        .header-wrapper {
+          flex-direction: column;
+        }
+        .Header {
+          max-height: 150px;
+        }
+      }
+
+      .input-pesquisa {
+        font-size: 1rem;
+        color: #333;
+        padding: 10px 25px;
+        width: 90%;
+        border: 1px solid #aaa;
+        border-radius: 10px;
+      }
+
+      .button-pesquisa {
+        margin-left: -35px;
+        font-size: 1.1rem;
+        color: #333;
+        background-color: transparent;
+        border: 0;
+        cursor: pointer;
       }
     `}
   </style>

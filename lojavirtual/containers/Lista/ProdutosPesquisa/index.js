460fe482ed47fda9cfac9c71fd79c0e9a49fb9e3
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-11-22 10:35:15 -0300

    Criando a página de pesquisa e componentes

---
+++ lojavirtual/containers/Lista/ProdutosPesquisa/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+import Produtos from '../../../components/Listas/Produtos'
+import Paginacao from '../../../components/Paginacao'
+
+const PRODUTOS = [
+  {
+    id: 69775244,
+    fotos: ['/static/img/mouse-1.png'],
+    titulo: 'Mouse Gamer 1',
+    preco: 35,
+    promocao: 25,
+  },
+  {
+    id: 52446977,
+    fotos: ['/static/img/mouse-2.png'],
+    titulo: 'Mouse Gamer 2',
+    preco: 55,
+    promocao: 45,
+  },
+  {
+    id: 44697752,
+    fotos: ['/static/img/mouse-3.png'],
+    titulo: 'Mouse Gamer 3',
+    preco: 65,
+    promocao: 55,
+  },
+]
+
+class ProdutosPesquisa extends Component {
+  state = { atual: 0 }
+
+  render() {
+    return (
+      <div className="container Categoria-Produtos">
+        <br />
+        <br />
+        <div className="flex flex-center">
+          <h1>Resultados para: Mouse</h1>
+        </div>
+        <Produtos produtos={PRODUTOS} itensPorLinha={4} />
+        <Paginacao
+          atual={this.state.atual || 0}
+          total={PRODUTOS.length * 4}
+          limite={PRODUTOS.length}
+          onClick={numeroAtual => this.setState({ atual: numeroAtual })}
+        />
+      </div>
+    )
+  }
+}
+
+export default ProdutosPesquisa
--- lojavirtual/pages/pesquisa.js
+++ lojavirtual/pages/pesquisa.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
+import Layout from '../components/Layout'
+import Cabecalho from '../containers/Cabecalho'
+import ProdutosPesquisa from '../containers/Lista/ProdutosCategoria'
+import Rodape from '../containers/Rodape'
+
 export default class Pesquisa extends Component {
   render() {
     return (
-      <div>
-        <h2>Loja IT - Pesquisa</h2>
-      </div>
+      <Layout title="Resultados para Mouse | Loja IT - Melhores produtos de tecnologia">
+        <Cabecalho />
+        <ProdutosPesquisa />
+        <Rodape />
+      </Layout>
     )
   }
 }

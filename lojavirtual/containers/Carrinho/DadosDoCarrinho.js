6af3310c044a332e2c3d88651804f0f4a7b03ba6
Alexander Alves <vendas77.rj@gmail.com>
2022-03-15 09:18:53 -0300

    Melhorando a estrutura da página de carrinho e componentes

---
+++ lojavirtual/containers/Carrinho/DadosDoCarrinho.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { formatMoney } from '../../utils'
+
+class DadosDoCarrinho extends Component {
+
+    renderDadosDoCarrinho() {
+        return (
+            <div className='flex-3'>
+                <div className='flex'>
+                    <div className='flex-1'>
+                        <p className='headline'>Valor do Pedido: </p>
+                    </div>
+                    <div className='flex-1 flex flex-right'>
+                        {formatMoney(85.35)}
+                    </div>
+                </div>
+                <br />
+                <Frete />
+                <div className='flex'>
+                    <div className='flex-1'>
+                        <p className='headline'>Valor Total: </p>
+                    </div>
+                    <div className='flex-1 flex flex-right'>
+                        {formatMoney(105.35)}
+                    </div>
+                </div>
+                <div className='flex flex-right'>
+
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        <div className='Dados-Do-Carrinho flex horizontal'>
+            <div className='flex-5'></div>
+            { this.renderDadosDoCarrinho() }
+        </div>
+    }
+}
+
+export default DadosDoCarrinho
